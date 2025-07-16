28ba274bdd5cd9ab8963793d6c2c23276bf86974
SyedMash <mashrtx7@gmail.com>
2025-07-16 07:21:31 -0700

    feat: add @inngest/agent-kit dependency and update input field in app router
    - Added @inngest/agent-kit version 0.9.0 to dependencies in package.json.
    - Changed input field name from 'text' to 'value' in the app router mutation for better clarity.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useState } from "react";
+
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -18,10 +23,11 @@ export default function Home() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         className="cursor-pointer"
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "Mashhood" })}
+        onClick={() => invoke.mutate({ value: value })}
       >
         Invoke Background Job
       </Button>
